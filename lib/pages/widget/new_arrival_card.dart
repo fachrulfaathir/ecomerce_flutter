7f064a0f4fae17fe81b017670f893675efc97820
Fachrul Faathirullah <fachrulfaathir36@gmail.com>
2026-05-25 14:30:01 +0700

    update homepage

--- lib/pages/widget/new_arrival_card.dart
+++ lib/pages/widget/new_arrival_card.dart
@@ -7,7 +7,9 @@ class NewArrivalCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      margin: EdgeInsets.only(bottom: 10),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             margin: EdgeInsets.only(right: 12, bottom: defaultMargin),
@@ -22,15 +24,18 @@ class NewArrivalCard extends StatelessWidget {
               child: Image.asset('assets/image_shoes.png', fit: BoxFit.cover,)),
           ),
           Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Football', style: secondaryTextStyle.copyWith(fontSize: 12)),
-                SizedBox(width: 6,),
-                Text('Predator 20.3 Firm Ground', style: primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),),
-                SizedBox(width: 6,),
-                Text('\$68,47' , style: priceTextStyle.copyWith(fontWeight: medium),)
-              ],
+            child: Container(
+              padding: EdgeInsets.only(top: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Football', style: secondaryTextStyle.copyWith(fontSize: 12)),
+                  SizedBox(height: 6,),
+                  Text('Predator 20.3 Firm Ground', style: primaryTextStyle.copyWith(fontSize: 16, fontWeight: semiBold),),
+                  SizedBox(height: 6,),
+                  Text('\$68,47' , style: priceTextStyle.copyWith(fontWeight: medium, fontSize: 14),)
+                ],
+              ),
             ),
           )
         ],
